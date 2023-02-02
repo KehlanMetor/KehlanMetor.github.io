f622d540feec763347c9d3c09c8d238a4136c98c
Pingu Telecom <82890272+KehlanMetor@users.noreply.github.com>
2023-02-02 20:55:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      /* Add your CSS here */
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+      }
+      h1 {
+        text-align: center;
+        color: #333;
+      }
+      .section {
+        padding: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Kehlan's IT and Computer Science Portfolio</h1>
+    <div class="section">
+      <!-- Add your HTML here -->
+      <h2>Introduction</h2>
+      <p>Hello, my name is Kehlan and I am an IT and computer science student. I am passionate about technology and love to explore new programming languages and software. In this portfolio, I will showcase some of my projects and experiences in the field.</p>
+    </div>
+  </body>
+</html>
